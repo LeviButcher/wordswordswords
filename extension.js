312e500e66ff249549f58a2cc5a8c5c931b9d423
Levi Butcher <levibutcher355@gmail.com>
2019-11-06 22:45:46 -0500

    Multi-Selection support for word count

--- extension.js
+++ extension.js
@@ -1,45 +1,48 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
+const wordCount = require("./lib/wordCount");
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
   const wordCountBar = vscode.window.createStatusBarItem(
     vscode.StatusBarAlignment.Right
   );
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
+  const updateToTextCount = text => {
+    const totalWords = wordCount(text);
+    wordCountBar.text = `Words: ${totalWords}`;
+    wordCountBar.show();
+  };
+
+  const updateToDocumentTotalTextCount = () => {
+    const text = vscode.window.activeTextEditor.document.getText();
+    updateToTextCount(text);
+  };
+
   let disposable = vscode.commands.registerCommand(
     "extension.fileWordCount",
     () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      const text = vscode.window.activeTextEditor.document.getText();
-      const length = text.split(" ").length;
-
-      wordCountBar.text = `Words: ${length}`;
-      wordCountBar.show();
-
-      vscode.workspace.onDidChangeTextDocument(l => {
-        const newText = l.document.getText();
-        const newLength = newText.split(" ").length;
-
-        wordCountBar.text = `Words: ${newLength}`;
+      vscode.workspace.onDidChangeTextDocument(() => {
+        updateToDocumentTotalTextCount();
       });
+
+      vscode.window.onDidChangeTextEditorSelection(e => {
+        if (e.selections[0].isEmpty) {
+          updateToDocumentTotalTextCount();
+          return;
+        }
+        const selectedWords = e.selections
+          .map(s => vscode.window.activeTextEditor.document.getText(s))
+          .join(" "); //ensure separation of selections
+
+        updateToTextCount(selectedWords);
+      });
+
+      updateToDocumentTotalTextCount();
     }
   );
   context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
